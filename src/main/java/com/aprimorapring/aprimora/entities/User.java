e233a5ee2f2dfe26eb3a140cd6b68b07e7b7cd3b
Gilson Oliveira <gilson_s.oliveira@hotmail.com>
2025-05-26 13:12:13 -0300

    Add conexão com o banco de dados H2

--- src/main/java/com/aprimorapring/aprimora/entities/User.java
+++ src/main/java/com/aprimorapring/aprimora/entities/User.java
@@ -1,5 +1,6 @@
 package com.aprimorapring.aprimora.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serial;
@@ -12,10 +13,14 @@ import java.io.Serializable;
 @ToString
 @EqualsAndHashCode
 @Builder
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
