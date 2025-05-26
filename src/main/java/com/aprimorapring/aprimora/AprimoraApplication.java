c37eef128e274c00c86ea80dd83893fb20fe6c1f
Gilson Oliveira <gilson_s.oliveira@hotmail.com>
2025-05-26 10:03:00 -0300

    Project created

---
+++ src/main/java/com/aprimorapring/aprimora/AprimoraApplication.java
@@ -0,0 +1,13 @@
+package com.aprimorapring.aprimora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprimoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprimoraApplication.class, args);
+	}
+
+}
