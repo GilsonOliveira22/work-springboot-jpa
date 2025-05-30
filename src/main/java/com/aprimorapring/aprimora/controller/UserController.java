5efd24448bcd46148ab62d87b9505ca4a22c8922
Gilson Oliveira <gilson_s.oliveira@hotmail.com>
2025-05-30 16:09:28 -0300

    Adicione o endpoint de atualização de usuário ao UserController
    .Implemente um endpoint PUT no UserController para atualizar os detalhes do usuário por ID. Adicionada a lógica correspondente de atualização no UserService para lidar com a modificação de dados e persistir as alterações. Isso inclui um método auxiliar para atualizar seletivamente os campos do usuário.

--- src/main/java/com/aprimorapring/aprimora/controller/UserController.java
+++ src/main/java/com/aprimorapring/aprimora/controller/UserController.java
@@ -40,4 +40,10 @@ public class UserController {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        User update = service.update(id, user);
+        return ResponseEntity.ok().body(update);
+    }
 }
