d7777462362b4b585b6360d48f1625caf49970b4
Gilson Oliveira <gilson_s.oliveira@hotmail.com>
2025-05-30 16:12:25 -0300

    Corrigir a indentação no método main do AprimoraApplication.
    javaAjustei a indentação no método main para melhorar a legibilidade do código e cumprir com os padrões de formatação. Nenhuma mudança funcional foi feita.

--- src/main/java/com/aprimorapring/aprimora/AprimoraApplication.java
+++ src/main/java/com/aprimorapring/aprimora/AprimoraApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AprimoraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AprimoraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AprimoraApplication.class, args);
+    }
 
 }
