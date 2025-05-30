e0e27bc0ae35f4c21094b522269736da1a429b70
Gilson Oliveira <gilson_s.oliveira@hotmail.com>
2025-05-30 16:35:43 -0300

    Adicione tratamento global de exceção para ResourceNotFoundException
    Introduziu `ResourceExceptionHandler` para lidar com `ResourceNotFoundException` com um formato de resposta de erro padronizado utilizando `StandardError`. Atualizou `UserService` para lançar `ResourceNotFoundException` quando um ID de usuário não é encontrado. Isso garante uma melhor gestão de erros em toda a aplicação.

---
+++ src/main/java/com/aprimorapring/aprimora/controller/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.aprimorapring.aprimora.controller.exceptions;
+
+import com.aprimorapring.aprimora.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler extends RuntimeException {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String erro = "Resource not found for ";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), erro, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/aprimorapring/aprimora/services/UserService.java
+++ src/main/java/com/aprimorapring/aprimora/services/UserService.java
@@ -2,6 +2,7 @@ package com.aprimorapring.aprimora.services;
 
 import com.aprimorapring.aprimora.entities.User;
 import com.aprimorapring.aprimora.repositories.UserRepository;
+import com.aprimorapring.aprimora.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
